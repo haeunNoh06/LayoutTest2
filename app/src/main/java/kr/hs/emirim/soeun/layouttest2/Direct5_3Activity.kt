128ef4d2d1498ba6410229c47bff9ba6bac68492
haeunNoh06 <okhagrace06@naver.com>
2023-05-30 20:06:44 +0900

    직접 풀어보기 5-3 완성

--- app/src/main/java/kr/hs/emirim/soeun/layouttest2/Direct5_3Activity.kt
+++ app/src/main/java/kr/hs/emirim/soeun/layouttest2/Direct5_3Activity.kt
@@ -4,26 +4,41 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class Direct5_3Activity : AppCompatActivity() {
+
+    lateinit var edit : EditText
+    lateinit var text : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         var linear1 = LinearLayout(this)
         linear1.orientation = LinearLayout.VERTICAL
-        linear1.setBackgroundColor(Color.rgb(255,255,0))
         val params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT)
         setContentView(linear1, params)
 
+        edit = EditText(this)
+        edit.hint = "Enter any text..."
+        edit.textSize = 20f
+        linear1.addView(edit)
+
         var btn = Button(this)
-        btn.text = "클릭해보세요"
-        btn.setBackgroundColor(Color.rgb(255,0,255))
+        btn.text = "버튼입니다"
         linear1.addView(btn)
 
+        text = TextView(this)
+        text.text = "Part of result"
+        text.textSize = 20f
+        text.setTextColor(Color.rgb(128,0,128))
+        linear1.addView(text)
+
         btn.setOnClickListener {
-            Toast.makeText(applicationContext, "클래스에서 생성된 버튼입니다.", Toast.LENGTH_LONG).show()
+            text.text = edit.text
         }
     }
 }
